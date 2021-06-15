0836bb72977cfd51c78550a37c9a16896a196f78
Rafael <faelzsouza@gmail.com>
2021-06-15 12:06:40 -0300

    Exercício 1

--- Aula_06_Exercícios_For.py
+++ Aula_06_Exercícios_For.py
@@ -1,8 +1,33 @@
 # 01 - Dado uma string com uma frase informada pelo usuário (incluindo espaços em branco), conte 
 # quantas vezes aparece as vogais a,e,i,o,u
+import unidecode
 
 string = input("Digite uma frase:\n")
+stringSemAcento = unidecode.unidecode(string)
+vogalA = 0
+vogalE = 0
+vogalI = 0
+vogalO = 0
+vogalU = 0
 
+for l in stringSemAcento:
+    if l.lower() == "a":
+        vogalA += 1
+    elif l.lower() == "e":
+        vogalE += 1
+    elif l.lower() == "i":
+        vogalI += 1
+    elif l.lower() == "o":
+        vogalO += 1
+    elif l.lower() == "u":
+        vogalU += 1
+
+print(f"Sua frase tem:")
+print(f"{vogalA} vogal(is) A")
+print(f"{vogalE} vogal(is) E")
+print(f"{vogalI} vogal(is) i")
+print(f"{vogalO} vogal(is) O")
+print(f"{vogalU} vogal(is) U")
 
 # 02 - Desenvolva um código que pergunte um número n para o usuário e exiba todos seus divisores
 
