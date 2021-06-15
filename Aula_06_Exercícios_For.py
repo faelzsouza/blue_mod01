f29b309722305f97da8e8f142df643c454ce0c57
Rafael <faelzsouza@gmail.com>
2021-06-15 11:41:55 -0300

    Exercícios usando For

---
+++ Aula_06_Exercícios_For.py
@@ -0,0 +1,28 @@
+# 01 - Dado uma string com uma frase informada pelo usuário (incluindo espaços em branco), conte 
+# quantas vezes aparece as vogais a,e,i,o,u
+
+string = input("Digite uma frase:\n")
+
+
+# 02 - Desenvolva um código que pergunte um número n para o usuário e exiba todos seus divisores
+
+
+
+# 03 - Faça um script que o usuário escolha um número de início, um número de fim, e um número de
+# passo. O programa deve printar todos os números do intervalo entre início e fim, "pulando" de 
+# acordo com o intervalo passado.
+
+
+
+# 04 - Desenvolva um código em que o usuário vai entrar vários números e no final vai apresentar a 
+# soma deles (o usuário vai dizer quantos números serão informados antes de começar)
+
+
+
+# 05 - Faça um programa que mostre na tela uma contagem regressiva para o estouro de fogos de 
+# artifício, indo de 10 até 0, com uma pausa de 1 segundo entre eles.
+
+
+
+# 06 - Escreva um programa onde o usuário digita uma frase e essa frase retorna sem nenhuma vogal.
+
